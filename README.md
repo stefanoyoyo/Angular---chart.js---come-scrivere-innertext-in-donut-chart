e59db456d5aaede0b73305e37de6338ff0726217
stefanoyoyo <31388199+stefanoyoyo@users.noreply.github.com>
2023-03-14 17:03:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Angular---chart.js---come-scrivere-innertext-in-donut-chart
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ng2-charts-doughnut-centertext-p6tgxf)
---
+++ src/app/app.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { MultiDataSet, Label, PluginServiceGlobalRegistrationAndOptions } from 'ng2-charts';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // Doughnut
+  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100],
+    [50, 150, 120],
+    [250, 130, 70],
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
+  public doughnutChartPlugins: PluginServiceGlobalRegistrationAndOptions[] = [{
+    beforeDraw(chart) {
+      const ctx = chart.ctx;
+      const txt = 'Center Text';
+
+      //Get options from the center object in options
+      const sidePadding = 60;
+      const sidePaddingCalculated = (sidePadding / 100) * (chart.innerRadius * 2)
+
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'middle';
+      const centerX = ((chart.chartArea.left + chart.chartArea.right) / 2);
+      const centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 2);
+
+      //Get the width of the string and also the width of the element minus 10 to give it 5px side padding
+      const stringWidth = ctx.measureText(txt).width;
+      const elementWidth = (chart.innerRadius * 2) - sidePaddingCalculated;
+
+      // Find out how much the font can grow in width.
+      const widthRatio = elementWidth / stringWidth;
+      const newFontSize = Math.floor(30 * widthRatio);
+      const elementHeight = (chart.innerRadius * 2);
+
+      // Pick a new font size so it will not be larger than the height of label.
+      const fontSizeToUse = Math.min(newFontSize, elementHeight);
+
+      ctx.font = fontSizeToUse + 'px Arial';
+      ctx.fillStyle = 'blue';
+
+      // Draw text in center
+      ctx.fillText('Center Text', centerX, centerY);
+    }
+  }];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  // events
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+}
